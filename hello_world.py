b6c58b737a407eff18b7b10ccb4b6b8c3a58aafa
TomerT <tomert1212@gmail.com>
2024-04-22 10:11:16 +0300

    addded Body class and Sphere

--- hello_world.py
+++ hello_world.py
@@ -1,53 +1,66 @@
 import bpy
 
-MS = 10**-3  # miliseconds
+MS = 10**-3  # milliseconds
 TICK = 1/60
 
 class Body:
-    ...
-
-class Cube(Body):
-    def __init__(self, name, size, x, y, z) -> None:
+    def __init__(self, name, size, x, y, z):
         self.size = size
         self.name = name
         self.velocity = 0
 
         if self.name in bpy.data.objects:
-            # If it exists, get the object
-            obj = bpy.data.objects[self.name]
-            # Reset its location to (0, 0, 0)
-            obj.location = (x, y, size + z)
-        else:
-            # If it doesn't exist, create a new cube primitive
-            bpy.ops.mesh.primitive_cube_add(size=self.size, location=(x, y, size + z))
-            # Rename the object to "Cube"
-            bpy.context.object.name = self.name
+            # If it exists, remove it
+            bpy.data.objects[self.name].select_set(True)
+            bpy.ops.object.delete()
+            # If it doesn't exist, create a new shape primitive
+        
+        self.create_primitive(size, (x, y, size + z))
 
-        self.obj = bpy.data.objects.get(name)
-        self.obj_location = self.obj.location
+        self.obj = bpy.data.objects.get(self.name)
+
+    def create_primitive(self, size, location):
+        raise NotImplementedError("create_primitive method not implemented")
 
     def move_location(self, x=0, y=0, z=0):
-        self.obj_location.x += x
-        self.obj_location.y += y
-        self.obj_location.z += z
+        self.obj.location.x += x
+        self.obj.location.y += y
+        self.obj.location.z += z
+
+
+class Cube(Body):
+    def create_primitive(self, size, location):
+        bpy.ops.mesh.primitive_cube_add(size=size, location=location)
+        bpy.context.object.name = self.name
+
+class Sphere(Body):
+    def create_primitive(self, size, location):
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=size/2, location=location)
+        bpy.context.object.name = self.name
 
 
 def gravity():
     GRAVITY = -9.8  # m/sec^2
-    loc = CUBE.obj_location
+
+    for object in OBJECTS:
+        loc = object.obj.location
     # g= dv/dt
     # dx = v * dt
     # dx = g * dt^2
 
     # Collision with floor
-    if loc.z - (CUBE.size // 2) > 0:
-        CUBE.velocity += GRAVITY * TICK
-        dz = CUBE.velocity * TICK
-        CUBE.move_location(z=dz)
-
+        if loc.z - (object.size // 2) > 0:
+            object.velocity += GRAVITY * TICK
+            dz = object.velocity * TICK
+            object.move_location(z=dz)
 
     return TICK # Seconds
+# Example usage
+OBJECTS = []
+CUBE = Cube("my_cube", size=2, x=0, y=5, z=10)
+SPHERE = Sphere("my_sphere", size=3, x=0, y=0, z=10)
+OBJECTS.append(CUBE)
+OBJECTS.append(SPHERE)
 
-CUBE = Cube("my_cube", size=2, x=0, y=0, z=1000)
 
 bpy.app.timers.register(gravity)
